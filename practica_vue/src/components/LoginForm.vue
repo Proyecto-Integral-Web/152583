c75a4603385e6af88ddd8ad9c61f84cc04fa61c7
Norma Lopez <60152609+noriloga@users.noreply.github.com>
2020-02-06 11:09:05 -0700

    inicio de login
    Formulario de inicio en práctica de clase

---
+++ practica_vue/src/components/LoginForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <section>
+
+    <form role="form">
+      <div class="form-group">
+        <label for="ejemplo_email_1">E-mail Address</label>
+        <input
+          type="email"
+          class="form-control"
+          id="ejemplo_email_1"
+          placeholder="E-mail address"
+        >
+      </div>
+      <div class="form-group">
+        <label for="ejemplo_password_1">Password</label>
+        <input
+          type="password"
+          class="form-control"
+          id="ejemplo_password_1"
+          placeholder="Password"
+        >
+        <label for="ejemplo_password_1">Don't have an account? Greate Account</label><br>
+        <button
+          type="button"
+          class="btn btn-danger btn-block"
+        >Login</button><br>
+        <button
+          type="button"
+          class="btn btn-outline-primary btn-block"
+        >Login whit Facebook</button><br>
+        <label for="ejemplo_password_1">Clase de Proyecto Integral Web 2020</label>
+
+      </div>
+    </form>
+  </section>
+</template>
+//Aqui ubicamos todo el javascript de nuestros componentes o views
+<script lang="js">
+export default {
+  name: 'LoginForm'
+}
+</script>
+//Style, ubicamos el css relacionado a nuestros views y componentes
+<style lang="scss"></style>
---
+++ practica_vue/src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <h1>Login</h1>
+    <loginForm></loginForm>
+  </div>
+</template>
+
+<script lang="js">
+
+import LoginForm from '@/components/LoginForm'
+export default {
+  name: 'login',
+  components: {
+    LoginForm
+  }
+}
+</script>
+<style lang="scss"></style>
